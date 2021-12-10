a24dcc399529eefdae0c743b1027bac3e85adff3
한상선 <hs94122@naver.com>
2021-12-10 17:12:18 +0900

    2021-12-10 17:12

--- src/echo/ex01/Client.java
+++ src/echo/ex01/Client.java
@@ -1,10 +1,56 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// 1.1 TCP 소켓 프로그래밍 - 클라이언트 만들기
 
+		//3. ~클라이언트 소켓 생성~
+		Socket socket = new Socket();
+		
+		System.out.println("<클라이언트 시작>");
+		System.out.println("====================");
+		
+		System.out.println("[서버에 연결을 요청합니다.]");
+		
+		//4. ~connect하기~
+		socket.connect(new InetSocketAddress("192.168.0.52", 10001));
+		//유동아이피를 적음
+		
+		System.out.println("서버에 연결되었습니다.");
+		
+		//문자열을 서버로 보내자
+		//소켓이 가지고 있는 메소드를 사용하여 보낸다.
+		OutputStream os = socket.getOutputStream();//주스트림
+		OutputStreamWriter osw = new OutputStreamWriter(os,"UTF-8");//번역해주는애
+		BufferedWriter bw = new BufferedWriter(osw);//출력 높여주는 애
+		
+		//메세지를 받아주는 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is,"UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		
+		//메세지 보내기
+		String str = "안녕하세요";
+		bw.write(str);
+		bw.newLine();
+		bw.flush();//보내주는애, 굳이 안써줘도 되지만 안쓰면 오류날때도 있기때문에 그냥 써줌
+		
+		//메세지 받기
+		String remsg = br.readLine();
+		System.out.println("server:"+remsg);
+		
+		bw.close();
+		socket.close();
 	}
-
 }
--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -1,10 +1,65 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {//예외처리를 위한 경고문을 제거해줌.
+		// 1.1 TCP 소켓 프로그래밍 - 서버, 서버소켓 만들기.
+		
+		//1. ~서버 소켓을 메모리에 올린다~
+		//서버소켓:클라이언트의 연결요청을 기다리면서 연결 요청에 대한 수락을 담당한다.
+		//소켓 : 클라이언트와 통신을 직접 담당한다.
+		//
+		//서버소켓 클래스가 이것들을 셋팅해주는 메소드를 가지고 있다.
+		//
+		//맥 아이피 얻는법 : 터미널에 ipconfig getifaddr en0
+		ServerSocket serverSocket = new ServerSocket();
+		
+		//2. ~포트를 만든다
+		serverSocket.bind(new InetSocketAddress("192.168.0.52", 10001));
+		//자신의 유동아이피번호, 포트번호10001(강사님이 정해주심)
+		//bind() : 포트를 생성해줌.
+		
+		System.out.println("<서버시작>");
+		System.out.println("================================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+		
+		//5. ~accept~ : connect가 오면 소켓을 만들어 줌
+		Socket socket = serverSocket.accept();
+		
+		System.out.println("[클라이언트에 연결되었습니다]");
+		
+		//메세지를 받아주는 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		
+		//메세지를 보내주는 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		//메세지 받기
+		String msg = br.readLine();
+		System.out.println("받은메세지:"+msg);
+		
+		//메세지 보내기
+		bw.write(msg);
+		bw.newLine();
+		bw.flush();
+		
+		bw.close();
+		socket.close();
+		serverSocket.close();	
 	}
-
 }
